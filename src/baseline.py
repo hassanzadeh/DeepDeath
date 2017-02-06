e3c7f76baf8a517bf5d15d936d2f22805170f400
Hamid Reza Hassanzadeh <hassanzadeh@gatech.edu>
2017-02-06 17:48:13 -0500

    up

--- src/baseline.py
+++ src/baseline.py
@@ -1,6 +1,7 @@
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.datasets import load_svmlight_file
+from sklearn.linear_model import LogisticRegression
 from random import shuffle
 
 
@@ -12,16 +13,18 @@ X_train, y_train = load_svmlight_file(trainfile)
 X_test, y_test = load_svmlight_file(testfile)
 
 
-skip = 10
+skip = 3
 #subsampling with skip rate 'skip'
 X_train, y_train = X_train[::skip],y_train[::skip]
 X_test, y_test = X_test[::skip],y_test[::skip]
 
 print('begin knn\n')
 #Note: you have to set n_jobs large enough to spread the tasks, otherwise it will crash
-knn = KNeighborsClassifier(n_jobs=1)
-knn.fit(X_train, y_train)
-print('predict\n')
-y_pred = knn.predict(X_test)
+#clf= KNeighborsClassifier(n_jobs=10)
+clf= LogisticRegression(n_jobs=1)
+print('Training\n')
+clf.fit(X_train, y_train)
+print('Predicting\n')
+y_pred = clf.predict(X_test)
 
 print(accuracy_score(y_test,y_pred))
